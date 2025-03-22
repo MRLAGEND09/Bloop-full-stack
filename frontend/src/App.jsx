43a9da9e6e0db36a7c7ec82871a921d6f0136306
IRFAN AHMED <Mrlagend09@gmail.com>
2025-03-22 17:21:19 +0600

    ed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,7 +15,7 @@ import Verify from './pages/verify.jsx'
 import Navbar from './components/navbar.jsx';
 import SearchBar from './components/searchBar.jsx';
 import Footer from './components/footer.jsx';
-const App = () => {
+const app = () => {
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
       <ToastContainer />
@@ -39,4 +39,4 @@ const App = () => {
   )
 }
 
-export default App
+export default app
