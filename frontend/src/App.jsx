99ca58736e9dfdbc5bb512687b1bceeb39a44bf4
IRFAN AHMED <Mrlagend09@gmail.com>
2025-03-22 15:21:38 +0600

    edit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -23,7 +23,7 @@ const App = () => {
       <Navbar />
       <SearchBar />
       <Routes>
-        <Route path='/' element={<Home/>} />
+        <Route path='/home' element={<Home/>} />
         <Route path='/collection' element={<Collection/>} />
         <Route path='/about' element={<About/>} />
         <Route path='/contact' element={<Contact/>} />
