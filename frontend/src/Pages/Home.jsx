6f9f3664ebc9ea45ca607f148463c213e64b6d2b
MRLAGEND09 <mrlagend09@gmail.com>
2026-03-12 14:06:28 +0600

    fix

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import Hero from '../Components/Hero'
+import Hero from '../components/Hero'
 import LatestCollection from '../components/LatestCollection'
 import BestSeller from '../components/BestSeller'
-import OurPolicy from '../Components/OurPolicy'
+import OurPolicy from '../components/OurPolicy'
 import NewsletterBox from '../components/NewsletterBox'
 
 const Home = () => {
